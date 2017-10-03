31123244c79f489190dda9089cf96e4e3fdfb328
FroChr123 <user12573ba@web.de>
2017-10-03 14:05:56 +0200

    Merged changes from Lab SS17

--- environment/src/main/java/simulation/environment/geometry/splines/StreetInterpolator.java
+++ environment/src/main/java/simulation/environment/geometry/splines/StreetInterpolator.java
@@ -11,6 +11,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * This class is a helper class for pedestrians to calculate the movement.
+ *
+ * Note: nearly all calculations will be based on all splines of the street. The movement will always
+ *       be between 2 spline points
+ */
 public class StreetInterpolator {
 
     private static final double TOLERANCE = 1E-9;
@@ -21,6 +27,11 @@ public class StreetInterpolator {
         return new Line(v1, v2, TOLERANCE);
     }
 
+    /**
+     * @param l1 first line
+     * @param l2 second line
+     * @return the intersection point of both lines (z will always be zero)
+     */
     private Point3D getIntersectionPoint(Line l1, Line l2) {
         Vector2D intersection = l1.intersection(l2);
         return new Point3D(
@@ -30,25 +41,42 @@ public class StreetInterpolator {
         );
     }
 
+    /**
+     * This class holds all information about the movement between two EnvNode Points
+     */
     private class MovementBetweenSplinePoints {
+
+        /** Starting point of the pavement "left" */
         private Point3D mBasePointPreLeft;
+        /** Ending point of the pavement "left" */
         private Point3D mBasePointNextLeft;
 
+        /** Starting point of the pavement "right" */
         private Point3D mBasePointPreRight;
+        /** Ending point of the pavement "right" */
         private Point3D mBasePointNextRight;
 
+        /** Direction for walking on the pavement */
         private Point3D mDirectionVector;
+        /** Direction for crossing the street */
         private Point3D mOrthogonalVector;
 
+        /** Center point between the start and the end point */
         private Point3D mCenterOfMovement;
 
+        /** Line for the "left" pavement */
         private Line mLeftLine;
+        /** Line for the "right" pavement */
         private Line mRightLine;
 
+        /** Boolean indicating that at the start of the line the left is the shorter one */
         private boolean mPreShorterSideIsLeft;
+        /** Boolean indicating that at the end of the line the left is the shorter one */
         private boolean mNextShorterSideIsLeft;
 
+        /** The last point at which we can cross the street safely (side dependes on "mPreShorterSideIsLeft") */
         private Point3D mPreLastCrossingPoint;
+        /** The last point at which we can cross the street safely (side dependes on "mNextShorterSideIsLeft") */
         private Point3D mNextLastCrossingPoint;
 
         MovementBetweenSplinePoints(Point3D basePre, Point3D baseNext) {
@@ -63,12 +91,20 @@ public class StreetInterpolator {
         }
     }
 
+    /** The street on which we want to move */
     private GeomStreet mBaseStreet;
+
+    /**
+     * List of all movement vectors between the nodes; if the street has n nodes, this list contains n-1 datasets
+     * This means index 0 is for the movement between EnvNode 0 and EnvNode 1, index 1 for EnvNode 1 and EnvNode 2
+     */
     private List<MovementBetweenSplinePoints> mSplineDirections;
 
+    // These two are internal helpers for the movement and should probably be kept in the PedestrianStreetParameters
     private int mMovingDirection = -1;
     private Point3D mCrossingStartPoint = null;
 
+    //  The width of the street with pavements
     private double mStreetAndPavementWidth = 8;
 
     public StreetInterpolator(GeomStreet base) {
@@ -78,6 +114,11 @@ public class StreetInterpolator {
         precomputePavements();
     }
 
+    /**
+     * This method will spawn a pedestrian on a random place for this street
+     * @param random the random class to use
+     * @return the generated parameters
+     */
     public PedestrianStreetParameters spawnAtRandomLocation(Random random) {
         mMovingDirection = random.nextInt(mSplineDirections.size());
         MovementBetweenSplinePoints curMovement = mSplineDirections.get(mMovingDirection);
@@ -88,6 +129,7 @@ public class StreetInterpolator {
 
         Point3D curPosition;
 
+        // We need to differentiate between the left and the right side since they may not have the same length
         if (isOnLeftPavement) {
             double maxDistance = curMovement.mBasePointPreLeft.distance(curMovement.mBasePointNextLeft);
             curPosition = curMovement.mBasePointPreLeft.add(
@@ -108,6 +150,10 @@ public class StreetInterpolator {
         );
     }
 
+    /**
+     * This method will precompute many movement vectors and borders to ensure we do not have to do the heavy lifting
+     * during the simulation
+     */
     private void precomputePavements() {
         List<EnvNode> envNodeList = mBaseStreet.getObject().getNodes();
 
@@ -130,6 +176,7 @@ public class StreetInterpolator {
             msp.mLeftLine = getLineBetweenPoints(msp.mBasePointPreLeft, msp.mBasePointNextLeft);
             msp.mRightLine = getLineBetweenPoints(msp.mBasePointPreRight, msp.mBasePointNextRight);
 
+            // Test if we can link this segment with a previous one; only works when we have already calculated one
             if (i > 0) {
                 MovementBetweenSplinePoints preMsp = mSplineDirections.get(i - 1);
 
@@ -141,6 +188,7 @@ public class StreetInterpolator {
                 preMsp.mBasePointNextRight = rightLineIntersection;
                 msp.mBasePointPreRight = rightLineIntersection;
 
+                // Now check which side of the pavement is the shorter one
                 boolean leftIsFarerAway = leftLineIntersection.distance(msp.mCenterOfMovement)
                         > rightLineIntersection.distance(msp.mCenterOfMovement);
 
@@ -162,6 +210,14 @@ public class StreetInterpolator {
         }
     }
 
+    /**
+     * Will calculate the new movement of the pedestrian
+     *
+     * Note: at the moment the result of this method also depends on some internal variables
+     * @param preParams the parameters in the frame before
+     * @param distance how far can we move
+     * @return the newly calculated parameters
+     */
     public PedestrianStreetParameters calculateNewMovement(PedestrianStreetParameters preParams, double distance) {
         Point3D curPosition = preParams.getPosition();
         boolean directionForward = preParams.isDirection();
@@ -176,11 +232,12 @@ public class StreetInterpolator {
 
         MovementBetweenSplinePoints curMovement = mSplineDirections.get(mMovingDirection);
 
-        // Test if when we cross the street if we even can cross it
+        // Test if we are allowed to cross the street
         if (isCrossingTheStreet &&
                 (mCrossingStartPoint != null || canCrossStreetAtPosition(curPosition, isOnLeftPavement, curMovement))) {
             Point3D crossingDirection = curMovement.mOrthogonalVector;
 
+            // Set up the movement vector in the correct direction
             if (isOnLeftPavement) {
                 crossingDirection = crossingDirection.multiply(-1);
             }
@@ -211,9 +268,11 @@ public class StreetInterpolator {
             );
         }
 
+        // This code below will calculate a movement on the pavement (e.g no crossing)
         Point3D direction = curMovement.mDirectionVector;
         Point3D targetingPoint = getTargetingPoint(directionForward, isOnLeftPavement, curMovement);
 
+        // Account for direction
         if (!directionForward) {
             direction = direction.multiply(-1);
         }
@@ -224,6 +283,7 @@ public class StreetInterpolator {
         double preDistance = targetingPoint.distance(curPosition);
         double newDistance = targetingPoint.distance(newPosition);
 
+        // Check if we need to turn around now since we reached the end of the street
         if (newDistance >= preDistance) {
             newPosition = targetingPoint;
 
@@ -243,6 +303,13 @@ public class StreetInterpolator {
         );
     }
 
+    /**
+     * Returns the point we want to move to
+     * @param directionForward the direction we want to move
+     * @param isOnLeftPavement if we are on the left pavement
+     * @param curMovement the current movement we are executing
+     * @return the point calculated
+     */
     private Point3D getTargetingPoint(boolean directionForward, boolean isOnLeftPavement,
                                       MovementBetweenSplinePoints curMovement) {
         if (directionForward) {
@@ -252,6 +319,13 @@ public class StreetInterpolator {
         }
     }
 
+    /**
+     * Checks if we should not cross the street here
+     * @param curPosition our position
+     * @param endPoint the point we want to reach
+     * @param lastCrossingPoint the last safe crossing point
+     * @return if we cannot cross here safely
+     */
     private boolean isInNonCrossingZone(Point3D curPosition, Point3D endPoint, Point3D lastCrossingPoint) {
         double distanceNonCrossing = endPoint.distance(lastCrossingPoint);
         double distanceToEnd = endPoint.distance(curPosition);
@@ -259,6 +333,13 @@ public class StreetInterpolator {
         return distanceToEnd <= distanceNonCrossing;
     }
 
+    /**
+     * Checks if we can cross the street safely
+     * @param curPosition our position
+     * @param isOnLeftPavement the side on which pavement we are currently
+     * @param msp our movement atm
+     * @return if we can safely cross here
+     */
     private boolean canCrossStreetAtPosition(Point3D curPosition, boolean isOnLeftPavement, MovementBetweenSplinePoints msp) {
         if (isOnLeftPavement) {
             if (msp.mPreShorterSideIsLeft && msp.mPreLastCrossingPoint != null
